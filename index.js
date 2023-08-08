c866d39200b569db968604e2d37766339e7c917e
Guillermo Rodas <me@guillermorodas.com>
2023-08-08 20:21:34 +0200

    Moving router

--- index.js
+++ index.js
@@ -1,59 +1,10 @@
 const express = require("express");
-const { loadMovies, generateId, saveMovies } = require("./helpers");
+const router = require("./routes/movies");
 
 const app = express();
 app.use(express.json());
 
-let movies = loadMovies();
-
-app.get("/movies", (req, res) => {
-  res.json(movies);
-});
-
-app.get("/movies/:id", (req, res) => {
-  const movieId = parseInt(req.params.id, 10);
-  const movieIndex = movies.findIndex((m) => m.id === movieId);
-
-  if (movieIndex === -1) {
-    return res.status(404).json({ error: "Movie not found" });
-  }
-  
-  res.json(movies[movieIndex]);
-});
-
-app.post("/movies", (req, res) => {
-  const newMovie = req.body;
-  newMovie.id = generateId(movies);
-  movies.push(newMovie);
-  saveMovies(movies);
-  res.json(newMovie);
-});
-
-app.put("/movies/:id", (req, res) => {
-  const movieId = parseInt(req.params.id, 10);
-  const movieIndex = movies.findIndex((m) => m.id === movieId);
-
-  if (movieIndex === -1) {
-    return res.status(404).json({ error: "Movie not found" });
-  }
-
-  Object.assign(movies[movieIndex], req.body);
-  saveMovies(movies);
-  res.json(movies[movieIndex]);
-});
-
-app.delete("/movies/:id", (req, res) => {
-  const movieId = parseInt(req.params.id, 10);
-  const movieIndex = movies.findIndex((m) => m.id === movieId);
-
-  if (movieIndex === -1) {
-    return res.status(404).json({ error: "Movie not found" });
-  }
-
-  movies.splice(movieIndex, 1);
-  saveMovies(movies);
-  res.json({ success: true });
-});
+app.use("/movies", router);
 
 const PORT = 3000;
 
